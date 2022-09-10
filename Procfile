666ab1e79f1eac3049290795e508899572bd584c
anushka <anushka30j@gmail.com>
2022-09-10 22:19:21 +0530

    "Updated"

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app --timeout 30 --workers 1 --threads 4 --log-level debug
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -27,6 +27,8 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("disable-dev-shm-usage")
+chrome_options.add_argument("window-size=1400,900")
+chrome_options.add_argument("--remote-debugging-port=9222")
 
 queue = Queuet()
 res = None
